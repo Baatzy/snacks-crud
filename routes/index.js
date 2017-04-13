fddd0f9611652bf81bb5e78fcc196000c022b3ce
STEFAN BAATZ <waltz.for.zizi@gmail.com>
2017-04-13 15:35:57 -0700

    Finished PUT and DELETE, and the redirect from homepage to INDEX

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Snack Reviews R US!' });
-});
+// Redirect to /snacks if navigating to home page
+router.get('/', (req, res, next) => {
+  db('snacks').then(snacks => {
+    res.redirect('snacks/index', { snacks })
+  })
+})
 
 module.exports = router;
--- routes/snacks.js
+++ routes/snacks.js
@@ -2,16 +2,19 @@ var express = require('express')
 var router = express.Router()
 var db = require('../db')
 
+// GET all snacks
 router.get('/', (req, res, next) => {
   db('snacks').then(snacks => {
     res.render('snacks/index', { snacks })
   })
 })
 
+//GET create snack form
 router.get('/new', (req, res, next) => {
   res.render('snacks/new')
 })
 
+// SHOW one snack
 router.get('/:id', (req, res, next) => {
   let id = req.params.id
   db('snacks').select('*').where({ id }).first().then(snack => {
@@ -19,6 +22,7 @@ router.get('/:id', (req, res, next) => {
   })
 })
 
+// CREATE snack
 router.post('/', (req, res, next) => {
   let newSnack = {
     name: req.body.name,
@@ -31,15 +35,34 @@ router.post('/', (req, res, next) => {
   })
 })
 
+// GET one snack from to edit
 router.get('/:id/edit', (req, res, next) => {
   let id = req.params.id
-  db('snacks').where({ id }).first().then(snack => {
+  db('snacks').select('*').where({ id }).first().then(snack => {
     res.render('snacks/edit', { snack })
   })
 })
 
-router.delete('/:id', (req, res, next) => {
+// POST edited snack
+router.put('/:id', (req, res, next) => {
+  let id = req.params.id
+  let snack = {
+    name: req.body.name,
+    rating: req.body.rating,
+    review_description: req.body['review-description'],
+    image_url: req.body['image-url']
+  }
+  db('snacks').update(snack, '*').where({ id }).then(snack => {
+    res.redirect(`/snacks/${snack[0].id}`)
+  })
+})
 
+// DESTROY one snack
+router.delete('/:id', (req, res, next) => {
+  let id = req.params.id
+  db('snacks').del().where({ id }).then(() => {
+    res.redirect('/snacks')
+  })
 })
 
 module.exports = router
